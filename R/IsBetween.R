d7112349531c492da2c63805caf29f9215578621
Robert Corty <rcorty@gmail.com>
2016-07-11 11:17:35 -0400

    added IsBetween function
    and documented thoroughly
    
    Signed-off-by: Robert Corty <rcorty@gmail.com>

---
+++ R/IsBetween.R
@@ -0,0 +1,37 @@
+#' @title IsBetween Function
+#' @name IsBetween
+#' @author Robert Corty
+#'
+#' @description This function returns \code{TRUE} if \code{x}
+#'    is between \code{lb} (lower bound) and \code{ub}
+#'    (upper bound).  This function can act on any type for which
+#'    \code{<, <=, >}, and \code{>=} are defined.  This function
+#'    works nicely with the pipe operator from \pkg{dplyr} and
+#'    \pkg{magrittr}.
+#'
+#' @param x the focal object
+#' @param lb lower bound
+#' @param ub upper bound
+#' @param equality.ok Indicates whether it acceptable for the focal
+#'    object to be equal to one of the arguments.  Defaults to
+#'    \code{FALSE}.
+#'
+#' @return \code{TRUE} if \code{x} is between \code{lb} and
+#'    \code{ub} and \code{FALSE} otherwise.
+#'
+#' @examples
+#' IsBetween(x = 0, lb = 1, ub = 2)
+#' IsBetween(x = 1, lb = 0, ub = 2)
+#' IsBetween(x = 0, lb = 0, ub = 2)
+#' IsBetween(x = 0, lb = 0, ub = 2, equality.ok = TRUE)
+#'
+#'
+#' @export
+#'
+IsBetween <- function(x, lb, ub, equality.ok = FALSE) {
+  if (equality.ok) {
+    return(x >= lb & x <= ub)
+  } else {
+    return(x > lb & x < ub)
+  }
+}
